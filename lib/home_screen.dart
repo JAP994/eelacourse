6c0ae9a81dbce8f810962b8504671ca0c0cb75e9
JAP994 <jonathansanchezponce@MacBook-Air-de-Jonathan.local>
2025-09-08 10:25:45 -0500

    View finalizada

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Flutter layout demo"), centerTitle: true),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.network(
             'https://imgs.search.brave.com/Gbyx2N-YaUzWSic7nWuqLcqD0hnOmvbbrH-uuLmh0wQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9nbG9i/ZXRyb3R0ZXJreWxl/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMi8wOS9EU0Nf/MDA2Ny5qcGc',
@@ -28,22 +29,54 @@ class HomeScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              spacing: 4,
               children: [
-                Column(children: [Icon(Icons.call), Text('CALL')]),
-                Column(children: [Icon(Icons.route), Text('ROUTE')]),
-                Column(children: [Icon(Icons.share), Text('SHARE')]),
+                Actions(icon: Icons.call, name: 'CALL'),
+                Actions(icon: Icons.route, name: 'ROUTE'),
+                Actions(icon: Icons.share, name: 'SHARE'),
               ],
             ),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
             child: Text(
-              "Quis enim officia incididunt labore ipsum. Voluptate reprehenderit adipisicing ex qui aliquip eu nostrud adipisicing incididunt cupidatat do. Esse ullamco culpa consectetur esse sunt ut.",
+              "Lorem Ipsum is simply dummy text"
+              " of the printing and typesetting "
+              "industry. Lorem Ipsum has been the "
+              "industry's standard dummy text ever "
+              "since the 1500s, when an unknown printer "
+              "took a galley of type and scrambled "
+              "it to make a type specimen book. It has "
+              "survived not only five centuries, but "
+              "also the leap into electronic typesetting,"
+              " remaining essentially unchanged. It was "
+              "popularised in the 1960s with the release "
+              "of Letraset sheets containing Lorem Ipsum "
+              "passages, and more recently with desktop "
+              "publishing software like Aldus PageMaker "
+              "including versions of Lorem Ipsum.",
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class Actions extends StatelessWidget {
+  const Actions({super.key, required this.icon, required this.name});
+
+  final IconData icon;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.deepPurple),
+        Text(name, style: TextStyle(color: Colors.deepPurple)),
+      ],
     );
   }
 }
